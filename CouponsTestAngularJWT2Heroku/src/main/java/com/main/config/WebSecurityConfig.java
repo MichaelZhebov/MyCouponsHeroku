261a350a1bedcfab91123c6660e6135e2037326a
MiMiSaR <mzhebov@gmail.com>
2020-01-13 21:40:52 +0200

    changed web configure

--- CouponsTestAngularJWT2Heroku/src/main/java/com/main/config/WebSecurityConfig.java
+++ CouponsTestAngularJWT2Heroku/src/main/java/com/main/config/WebSecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +18,10 @@ import com.main.service.UserDetailsServiceImpl;
  
  
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true
+)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
@@ -46,10 +53,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+    
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/register/*").permitAll()
